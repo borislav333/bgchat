4f43f2acfe6728f2386a98a2f4776b5c23c33d12
Borislav <b.yakimov@yahoo.com>
2020-10-04 23:01:01 +0300

    Updated configuration files

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,6 @@ REACT_APP = os.path.join(BASE_DIR, 'frontend')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP, 'build', 'static')
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
